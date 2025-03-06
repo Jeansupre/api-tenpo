a3f1f307071fd2a3ee3d88ef8527a0fe9d6464b1
Jean Carlo Rodriguez Sanchez <j.carlo0123@gmail.com>
2025-03-06 02:38:38 -0500

    Test: Se agregan algunas configuraciones de los test unitarios

--- src/test/java/com/tenpo/prueba/PruebaApplicationTests.java
+++ src/test/java/com/tenpo/prueba/PruebaApplicationTests.java
@@ -2,8 +2,10 @@ package com.tenpo.prueba;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest()
+@ActiveProfiles("test")
 class PruebaApplicationTests {
 
 	@Test
